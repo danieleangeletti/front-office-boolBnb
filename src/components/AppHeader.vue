252e0acf7cfe655465d7c77f728909185bdc9b7b
MattiaPaiano91 <147002842+MattiaPaiano91@users.noreply.github.com>
2024-03-27 10:20:15 +0100

    Initial commit

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+    data() {
+        return {
+            
+        };
+    },
+    methods: {
+
+    }
+}
+</script>
+
+<template>
+    <header>
+        HEADER
+    </header>
+</template>
+
+<style lang="scss" scoped>
+@use "../assets/scss/main.scss" as *;
+</style>
