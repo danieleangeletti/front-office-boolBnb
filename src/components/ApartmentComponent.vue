da29cc54a6c5b4d0587a4ee69065f6d1501b84cd
MattiaPaiano91 <paianomattia1@gmail.com>
2024-03-29 18:21:10 +0100

    ADD

--- src/components/ApartmentComponent.vue
+++ src/components/ApartmentComponent.vue
@@ -15,21 +15,25 @@ export default {
 </script>
 
 <template>
+    
     <div class="card">
-        <div class="img-box">
-            <img :src="apartment.img_cover_path" alt="Cover Image">
-        </div>
-        <ul class="p-0">
-            <li>
-                {{ apartment.name }}
-            </li>
-            <li>
-                <span>${{ Math.floor(apartment.price) }} </span><span >notte .</span><span class="night ms-1">${{ Math.floor(apartment.price * apartment.n_guests) }} in totale</span>
-            </li>
-        </ul>
+        <router-link class="link" :to="{name:'apartment-show', params: { slug: apartment.slug }}">
+            <div class="img-box">
+                <img :src="apartment.img_cover_path" alt="Cover Image">
+            </div>
+            <ul class="p-0">
+                <li>
+                    {{ apartment.name }}
+                </li>
+                <li>
+                    <span>${{ Math.floor(apartment.price) }} </span><span >notte .</span><span class="night ms-1">${{ Math.floor(apartment.price * apartment.n_guests) }} in totale</span>
+                </li>
+            </ul>
+        </router-link>
     </div>
 </template>
                 
+            
 
 
 <style lang="scss" scoped>
@@ -39,6 +43,10 @@ export default {
     margin-top: 50px;
     padding: 20px;
     border:none!important;
+    .link{
+        color: black;
+        text-decoration: none;
+    }
     .night{
         font-size: 0.9rem;
         color: gray;
