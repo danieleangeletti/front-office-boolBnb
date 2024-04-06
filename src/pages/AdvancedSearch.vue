44606975d3cebfb8193c041207273f0150b85dc2
danieleangeletti <angelettidaniele@gmail.com>
2024-04-06 10:17:14 +0200

    REMOVED form

--- src/pages/AdvancedSearch.vue
+++ src/pages/AdvancedSearch.vue
@@ -42,31 +42,29 @@ export default {
     <div class="container-fluid">
       <div class="row">
         <div class="col-12">
-            <form>
-                <div class="mt-3 mb-3 d-flex justify-content-center">
-                    <input v-model="nRooms" class="form-control w-50" placeholder="Minimum number of rooms"></input>
+            <div class="mt-3 mb-3 d-flex justify-content-center">
+                <input v-model="nRooms" class="form-control w-50" placeholder="Minimum number of rooms"></input>
+            </div>
+            <div class="mb-3 d-flex justify-content-center">
+                <input v-model="nBeds" class="form-control w-50" placeholder="Minimum number of beds"></input>
+            </div>
+            <div class="mb-3 d-flex justify-content-center">
+                <input v-model="radius" type="range" class="form-range w-50" id="customRange1" min="0" max="50" step="0.5">
+                <div>{{ radius }} km</div>
+            </div>
+            <div class="mb-3 d-flex justify-content-center">
+                <div v-for="(elem, i) in store.services"  class="form-check me-3">
+                    <input class="form-check-input" type="checkbox" :value="elem.type_of_service" id="flexCheckDefault">
+                    <label class="form-check-label" for="flexCheckDefault">
+                        {{ elem.type_of_service }}
+                    </label>
                 </div>
-                <div class="mb-3 d-flex justify-content-center">
-                    <input v-model="nBeds" class="form-control w-50" placeholder="Minimum number of beds"></input>
-                </div>
-                <div class="mb-3 d-flex justify-content-center">
-                    <input v-model="radius" type="range" class="form-range w-50" id="customRange1" min="0" max="50" step="0.5">
-                    <div>{{ radius }} km</div>
-                </div>
-                <div class="mb-3 d-flex justify-content-center">
-                    <div v-for="(elem, i) in store.services"  class="form-check me-3">
-                        <input class="form-check-input" type="checkbox" :value="elem.type_of_service" id="flexCheckDefault">
-                        <label class="form-check-label" for="flexCheckDefault">
-                            {{ elem.type_of_service }}
-                        </label>
-                    </div>
-                </div>
-                <div class="mb-3 d-flex justify-content-center">
-                    <button type="submit" class="btn btn-outline-dark m-2">
-                        SEARCH
-                    </button>
-                </div>
-            </form>
+            </div>
+            <div class="mb-3 d-flex justify-content-center">
+                <button type="submit" class="btn btn-outline-dark m-2">
+                    SEARCH
+                </button>
+            </div>
         </div>
       </div>
     </div>
