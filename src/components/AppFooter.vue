f28636ed1642d13134e0dd3d3095537d2d372da1
danieleangeletti <angelettidaniele@gmail.com>
2024-04-10 18:18:46 +0200

    PROCEDIMENTI footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -2,43 +2,352 @@
 export default {
   data() {
     return {
+      visibleDestinations: [],
+      destinationsObjs: [
+      [
+        {
+        destination: 'Milano',
+        typeOfStay: 'Soggiorni in appartamenti'
+        },
+        {
+          destination: 'Bergamo',
+          typeOfStay: 'Soggiorni in chalet'
+        },
+        {
+          destination: 'Rovereto',
+          typeOfStay: 'Soggiorni in casale'
+        },
+        {
+          destination: 'Venezia',
+          typeOfStay: 'Soggiorni in alloggi sull\'acqua',
+        },
+        {
+          destination: 'Firenze',
+          typeOfStay: 'Soggiorni in casale',
+        },
+        {
+          destination: 'Verona',
+          typeOfStay: 'Soggiorni in appartamenti',
+        },
+        {
+          destination: 'Lago di Garda',
+          typeOfStay: 'Soggiorni in alloggi sull\'acqua',
+        },
+        {
+          destination: 'Bolzano',
+          typeOfStay: 'Soggiorni in appartamenti',
+        },
+        {
+          destination: 'Genova',
+          typeOfStay: 'Soggiorni in appartamenti',
+        },
+        {
+          destination: 'Trento',
+          typeOfStay: 'Soggiorni in casale',
+        },
+        {
+          destination: 'Mantova',
+          typeOfStay: 'Soggiorni in chalet',
+        },
+        {
+          destination: 'Ferrara',
+          typeOfStay: 'Soggiorni in appartamenti',
+        },
+        {
+          destination: 'Lago d\'orta',
+          typeOfStay: 'Soggiorni in casale',
+        },
+        {
+          destination: 'Vicenza',
+          typeOfStay: 'Soggiorni in appartamenti',
+        },
+        {
+          destination: 'Bellagio',
+          typeOfStay: 'Soggiorni in chalet',
+        },
+      ],
+      [
+        {
+        destination: 'Sirmione',
+        typeOfStay: 'Soggiorni in case'
+        },
+        {
+          destination: 'Cinque Terre',
+          typeOfStay: 'Case vacanze'
+        },
+        {
+          destination: 'Portofino',
+          typeOfStay: 'Case vacanze'
+        },
+        {
+          destination: 'Santa Margherita Ligure',
+          typeOfStay: 'Soggiorni in alloggi sull\'acqua',
+        },
+        {
+          destination: 'Rapallo',
+          typeOfStay: 'Case vacanze',
+        },
+        {
+          destination: 'Viareggio',
+          typeOfStay: 'Soggiorni in appartamenti',
+        },
+        {
+          destination: 'Lido di Venezia',
+          typeOfStay: 'Soggiorni in alloggi sull\'acqua',
+        },
+        {
+          destination: 'Jesolo',
+          typeOfStay: 'Case vacanze',
+        },
+        {
+          destination: 'Rimini',
+          typeOfStay: 'Case vacanze',
+        },
+        {
+          destination: 'Sestri Levante',
+          typeOfStay: 'Soggiorni in appartamenti',
+        },
+        {
+          destination: 'Alassio',
+          typeOfStay: 'Soggiorni in chalet',
+        },
+        {
+          destination: 'Sanremo',
+          typeOfStay: 'Case vacanze',
+        },
+        {
+          destination: 'Cattolica',
+          typeOfStay: 'Soggiorni in appartamenti',
+        },
+        {
+          destination: 'Grado',
+          typeOfStay: 'Soggiorni in alloggi sull\'acqua',
+        },
+        {
+          destination: 'Finale Ligure',
+          typeOfStay: 'Case vacanze',
+        },
+      ],
+      [
+        {
+          destination: 'Isola d\'Elba',
+          typeOfStay: 'Soggiorni in alloggi sull\'acqua',
+        },
+        {
+          destination: 'Isola Bella',
+          typeOfStay: 'Case vacanze',
+        },
+        {
+          destination: 'Isola dei Pescatori',
+          typeOfStay: 'Case vacanze',
+        },
+        {
+          destination: 'Isola Comacina',
+          typeOfStay: 'Soggiorni in appartamenti',
+        },
+        {
+          destination: 'Isola di Burano',
+          typeOfStay: 'Soggiorni in chalet',
+        },
+        {
+          destination: 'Ischia',
+          typeOfStay: 'Soggiorni in appartamenti',
+        },
+        {
+          destination: 'Isola di San Giulio',
+          typeOfStay: 'Case vacanze',
+        },
+        {
+          destination: 'Isola di San Nicola',
+          typeOfStay: 'Soggiorni in chalet',
+        },
+        {
+          destination: 'Isola di San Domino',
+          typeOfStay: 'Soggiorni in appartamenti',
+        },
+        {
+          destination: 'Isola di Pianosa',
+          typeOfStay: 'Case vacanze',
+        },
+      ],
+      [
+        {
+          destination: 'Cortina d\'Ampezzo',
+          typeOfStay: 'Case vacanze'
+        },
+        {
+          destination: 'Livigno',
+          typeOfStay: 'Soggiorni in appartamenti'
+        },
+        {
+          destination: 'Bormio',
+          typeOfStay: 'Case vacanze'
+        },
+        {
+          destination: 'Courmayeur',
+          typeOfStay: 'Soggiorni in chalet'
+        },
+        {
+          destination: 'Sestriere',
+          typeOfStay: 'Soggiorni in chalet'
+        },
+        {
+          destination: 'Madonna di Campiglio',
+          typeOfStay: 'Soggiorni in appartamenti'
+        },
+        {
+          destination: 'Limone Piemonte',
+          typeOfStay: 'Case vacanze'
+        },
+        {
+          destination: 'Alagna Valsesia',
+          typeOfStay: 'Soggiorni in chalet'
+        },
+        {
+          destination: 'Macugnaga',
+          typeOfStay: 'Soggiorni in appartamenti'
+        },
+        {
+          destination: 'Sappada',
+          typeOfStay: 'Case vacanze'
+        },
+      ]
+      ],
       destinations: [
         "Popolari",
         "Sul litorale",
-        "Destinazioni storiche",
         "Isole",
         "Montagne",
-        "Categorie",
-        "Attività",
       ],
+      footerList: [
+        {
+          title: 'Assistenza',
+          list: ['Centro assistenza', 'AirCover', 'Antidiscriminazione', 'Supporto alla disabilità', 'Opzioni di cancellazione', 'Segnala problema nel quartiere']
+        },
+        {
+          title: 'Ospitare',
+          list: ['Apri un Boolbnb', 'AirCover per gli host', 'Risorse per gli host', 'Forum della community', 'Ospitare responsabilmente', 'Partecipa a una lezione gratuita sull\'ospitalità']
+        },
+        {
+          title: 'Boolbnb',
+          list: ['Newsroom', 'Nuove funzionalità', 'Opportunità di lavoro', 'Investitori', 'Gift card', 'Soggiorni con Boolbnb.org']
+        },
+      ],
+      activeIndex: 0,
     };
   },
-  methods: {},
+  methods: {
+    changeActiveIndex(i){
+      this.activeIndex = i;
+      this.visibleDestinations = [];
+      this.visibleDestinations.push(this.destinationsObjs[this.activeIndex]);
+    }
+  },
+  mounted(){
+    this.visibleDestinations.push(this.destinationsObjs[this.activeIndex]);
+  }
 };
 </script>
 
 <template>
   <footer class="footer-bg">
+    <div class="container pt-5 pb-5 special-height">
+      <div class="future-trips-container">
+        <h4 class="mb-4">Ispirazione per viaggi futuri</h4>
+        <ul class="list-unstyled mb-5">
+          <li @click="changeActiveIndex(i)" v-for="(elem, i) in destinations" class="d-inline-block pb-3 pt-3" :class="activeIndex == i ? 'active' : ''">
+            <span v-if="i == 0" class="ps-0 pe-3">{{ elem }}</span>
+            <span v-if="i > 0 && i < destinations.length - 1" class="ps-3 pe-3">{{
+              elem
+            }}</span>
+            <span v-if="i == destinations.length - 1" class="ps-3 pe-0">{{
+              elem
+            }}</span>
+          </li>
+        </ul>
+        <div class="row">
+          <div v-for="(elem, i) in visibleDestinations[0]" class="col-12 col-sm-6 col-md-4 col-lg-2 mb-3 pointer">
+            <div>
+              <strong>{{ elem.destination }}</strong>
+            </div>
+            <div class="text-secondary">
+              {{ elem.typeOfStay }}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <hr class="m-0">
     <div class="container">
-      <h4>Ispirazione per viaggi futuri</h4>
-      <ul class="list-unstyled">
-        <li v-for="(elem, i) in destinations" class="d-inline-block">
-          <span v-if="i == 0" class="ps-0 pe-3">{{ elem }}</span>
-          <span v-if="i > 0 && i < destinations.length - 1" class="ps-3 pe-3">{{
-            elem
-          }}</span>
-          <span v-if="i == destinations.length - 1" class="ps-3 pe-0">{{
-            elem
-          }}</span>
-        </li>
-      </ul>
+      <div class="row pt-5 pb-5">
+        <div v-for="(elem, i) in footerList" class="col-12 col-sm-6 col-md-4 pointer">
+          <div class="mb-3">
+            <strong>{{ elem.title }}</strong>
+          </div>
+          <div v-for="(elem, j) in footerList[i].list" class="mb-3">
+            {{ elem }}
+          </div>
+        </div>
+      </div>
+      <hr class="m-0">
+    </div>
+    <div class="container pt-4 pb-4">
+      <div class="d-flex justify-content-between">
+        <div>
+          <span class="pe-1">
+            © 2024 Airbnb, Inc.
+          </span>
+          <span class="ps-1 pe-1">
+            ·
+          </span>
+          <span class="pointer ps-1 pe-1">
+            Privacy
+          </span>
+          <span class="ps-1 pe-1">
+            ·
+          </span>
+          <span class="pointer ps-1 pe-1">
+            Termini
+          </span>
+          <span class="ps-1 pe-1">
+            ·
+          </span>
+          <span class="pointer ps-1 pe-1">
+            Mappa del sito
+          </span>
+          <span class="ps-1 pe-1">
+            ·
+          </span>
+          <span class="pointer ps-1">
+            Dettagli dell'azienda
+          </span>
+        </div>
+        <div>
+          LOL
+        </div>
+      </div>
     </div>
   </footer>
 </template>
 
 <style lang="scss" scoped>
 @use "../assets/scss/main.scss" as *;
+.pointer {
+  cursor: pointer;
+}
+.special-height {
+  min-height: 530px;
+}
 .footer-bg {
   background-color: #f7f7f7;
+  ul {
+    border-bottom: 1px solid #DDDDDD;
+    li {
+    cursor: pointer;
+  }
+  .active {
+    border-bottom: 3px solid black;
+  }
+  }
 }
 </style>
